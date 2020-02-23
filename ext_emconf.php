efdeeef508ebe1d9b5e9cf7d25afade3a8b63399
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-02-23 10:15:46 +0100

    [TASK] Adjust versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
     'state' => 'alpha',
-    'version' => '0.1.0',
+    'version' => '0.3.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.14-10.4.99',
             'schema' => '1.3.0-0.0.0',
         ],
     ],

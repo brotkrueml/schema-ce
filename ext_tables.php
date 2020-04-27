b2581f67c293f1a92accd64c3788ce2bf9001032
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-04-27 14:04:59 +0200

    [TASK] Simplify import of tsconfig files

--- ext_tables.php
+++ ext_tables.php
@@ -2,13 +2,11 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 (function ($extensionKey='schema_records') {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_schemarecords_domain_model_type');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_schemarecords_domain_model_property');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_schemarecords_domain_model_type');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_schemarecords_domain_model_property');
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-        @import "EXT:' . $extensionKey . '/Configuration/TSconfig/Page/"
-        @import "EXT:' . $extensionKey . '/Configuration/TSconfig/Page/Info/"
-        @import "EXT:' . $extensionKey . '/Configuration/TSconfig/Page/Presets/"
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        <INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $extensionKey . '/Configuration/TSconfig/Page/" extensions="tsconfig">
     ');
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1584554889] = [
